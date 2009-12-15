4bf266a934de9f5cc647846449b7945e89bc4176
Julian_Chu <walkingice@0xlab.org>
2009-12-15 19:31:28 +0800

    Enable alpha blending

--- src/org/zeroxlab/artywall/ViewManager.java
+++ src/org/zeroxlab/artywall/ViewManager.java
@@ -136,6 +136,9 @@ public class ViewManager {
 	    gl.glEnableClientState(GL10.GL_TEXTURE_COORD_ARRAY);
 	    gl.glEnable(gl.GL_CULL_FACE);
 
+	    gl.glEnable(gl.GL_BLEND);
+	    gl.glBlendFunc(gl.GL_SRC_ALPHA, gl.GL_ONE_MINUS_SRC_ALPHA);
+
 	    mManager.initGLViews(gl);
 	}
 
