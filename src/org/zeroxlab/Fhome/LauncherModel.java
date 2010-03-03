b6040ea529d77d2a7ec2a53a935c53efc4fd9ee7
Julian_Chu <walkingice@0xlab.org>
2010-03-03 13:55:13 +0800

    add LauncherModel

---
+++ src/org/zeroxlab/Fhome/LauncherModel.java
@@ -0,0 +1,54 @@
+/*
+ * Authored By Julian Chu <walkingice@0xlab.org>
+ *
+ * Copyright (c) 2009 0xlab.org - http://0xlab.org/
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package org.zeroxlab.Fhome;
+
+import android.util.Log;
+
+import android.content.res.Resources;
+import android.content.pm.PackageManager;
+
+public class LauncherModel {
+    private class ApplicationLoader implements Runnable {
+
+	private final Object mLock = new Object();
+	private boolean mRunning = false;
+
+	// The target which contains Application items
+	private GLObject mTarget;
+	private PackageManager mPackMgr;
+
+	LauncherModel(GLObject target, PackageManager packageManager) {
+	    mTarget  = target;
+	    mPackMgr = packageManager;
+	}
+
+	public void stop() {
+	    synchronized (mLock) {
+		mRunning = false;
+	    }
+	}
+
+	public void run() {
+
+	    while (mRunning)
+	}
+    }
+}
+
