dbcfb2b09d7db2d051d228331fb0226bd622086c
Julian_Chu <walkingice@0xlab.org>
2010-05-14 16:48:02 +0800

    Bubble: calculate size of bubble
    We might add bubble into different level

--- src/org/zeroxlab/Fhome/Bubble.java
+++ src/org/zeroxlab/Fhome/Bubble.java
@@ -37,12 +37,17 @@ public class Bubble extends GLObject {
     public static String background = "bubble";
     private Owner mOwner;
 
-    public final static float BORDER = 10f;
-    public final static float BUTTON_WIDTH  = 80f;
-    public final static float BUTTON_HEIGHT = 20f;
+    public final static int BORDER_PX = 15;
+    public final static int BUTTON_WIDTH_PX  = 80;
+    public final static int BUTTON_HEIGHT_PX = 20;
     public final static int BUTTON_TEXT_SIZE = 12;
     public final static int BUTTON_OK     = 1;
     public final static int BUTTON_CANCEL = 2;
+
+    protected int mLevel = ViewManager.LEVEL_POSTER;
+    protected float mButtonWidth  = 80f;
+    protected float mButtonHeight = 20f;
+    protected float mBorder  = 15f;
 
     GLLabel mLabel;
     public final static int mDefaultLabelColor = 0xFF000000;
@@ -72,8 +77,6 @@ public class Bubble extends GLObject {
 	mCancel.setBackground("button");
 	mOk.setVisible(false);
 	mCancel.setVisible(false);
-	mOk.setSize(BUTTON_WIDTH, BUTTON_HEIGHT);
-	mCancel.setSize(BUTTON_WIDTH, BUTTON_HEIGHT);
 	addChild(mOk);
 	addChild(mCancel);
 
@@ -83,11 +86,31 @@ public class Bubble extends GLObject {
 	float y = this.getHeight() - mCloseHeight;
 	mClose.setXY(x, y);
 	addChild(mClose);
+
+	setLevel(mLevel);
     }
 
     @Override
     public void setSize(float width, float height) {
 	super.setSize(width, height);
+	updateLayout();
+    }
+
+    public void setLevel(int level) {
+	mLevel = level;
+	mOk.setLevel(mLevel);
+	mCancel.setLevel(mLevel);
+	mLabel.setLevel(mLevel);
+	float tmp;
+	tmp = ViewManager.PROJ_WIDTH * BORDER_PX / ViewManager.mScreenWidth;
+	mBorder= ViewManager.convertToLevel(mLevel, tmp);
+	tmp = ViewManager.PROJ_WIDTH * BUTTON_WIDTH_PX / ViewManager.mScreenWidth;
+	mButtonWidth = ViewManager.convertToLevel(mLevel, tmp);
+	tmp = ViewManager.PROJ_HEIGHT * BUTTON_HEIGHT_PX / ViewManager.mScreenHeight;
+	mButtonHeight= ViewManager.convertToLevel(mLevel, tmp);
+
+	mOk.setSize(mButtonWidth, mButtonHeight);
+	mCancel.setSize(mButtonWidth, mButtonHeight);
 	updateLayout();
     }
 
@@ -122,12 +145,12 @@ public class Bubble extends GLObject {
 	mClose.setXY(x, y);
 
 	if (mShowOptions) {
-	    float bottomHeight = BUTTON_HEIGHT + BORDER * 2;
-	    x = center_x - BORDER - BUTTON_WIDTH;
-	    y = h - bottomHeight + BORDER;
+	    float bottomHeight = mButtonHeight + mBorder * 2;
+	    x = center_x - mBorder - mButtonWidth;
+	    y = h - bottomHeight + mBorder;
 	    mOk.setXY(x, y);
 
-	    x = center_x + BORDER;
+	    x = center_x + mBorder;
 	    mCancel.setXY(x, y);
 
 	    h = h - bottomHeight;
